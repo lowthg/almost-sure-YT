4dabfa8f08b4d0c6244b64a71e895d75ff3c2052
George Lowther <37965159+lowthg@users.noreply.github.com>
2025-08-03 10:07:07 +0100

    algan anims

--- algansource/bloch.py
+++ algansource/bloch.py
@@ -19,32 +19,34 @@ def bloch(r=1.):
     interior_col = GREEN_C
     # surface_col = Color("#333333")
     surface_col = Color("#000060")
+    r2 = r * 0.995
     s1 = Sphere(radius=r, color=surface_col, opacity=1).set_shader(basic_pbr_shader)
-    s2 = Sphere(radius=r * 0.995, color=interior_col, opacity=1).set_shader(null_shader)
-    s3 = ManimMob(mn.Sphere(radius=r * 1.005, fill_opacity=0, stroke_opacity=0.5, stroke_color=mn.BLACK, stroke_width=2)).orbit_around_point(ORIGIN, 90, RIGHT)
+    s2 = Sphere(radius=r2, color=interior_col, opacity=1).set_shader(null_shader)
+    s3 = ManimMob(mn.Sphere(radius=r * 1.005, fill_opacity=0, stroke_opacity=0.5, stroke_color=mn.BLACK, stroke_width=2))
+    s3.orbit_around_point(ORIGIN, 90, RIGHT)
+    s3.orbit_around_point(ORIGIN, 7, UP)  # don't cover the center with a line
+    s3.orbit_around_point(ORIGIN, 35, RIGHT)  # let's make the north pole point slightly towards us
     c1 = Cylinder(radius=0.2, height=1, color=WHITE)
     a = math.log(1 - min_op_s1) * r # min opacity = 1 - exp(a/r)
     b = math.log(1 - max_op_s2) / r  # max opacity = 1 - exp(b * r)
 
-    op = 1 - math.exp(b * r/2)
+    op = 1 - math.exp(b * r2/2)
+    center_dot = Sphere(radius=r * 0.04, color=WHITE * (1-op) + interior_col * op)
 
-    center_dot = Sphere(radius=r * 0.04, color=WHITE * op + interior_col * (1-op))
-
-    obj_color = WHITE
+    c1.base_color = WHITE
 
     def interior_col_update(obj: Mob, t):
         print('updator called')
+        col = obj.base_color
         for p in obj.get_descendants():
             loc = p.location
-            col = obj_color
-            nm = loc.norm(p=2, dim=-1, keepdim=True)
-            inside = nm.le(r)
-            z = (1 - loc[...,:2].square().sum(dim=-1, keepdim=True)).clamp(0).sqrt()
+            nm = loc.norm(dim=-1, keepdim=True)
+            inside = nm.le(r2)
+            z = (r2*r2 - loc[...,:2].square().sum(dim=-1, keepdim=True)).clamp(0).sqrt()
             depth = z + loc[...,2:3]
             inside_op = 1 - (depth * (b/2)).exp()
             new_col = torch.where(inside, col * (1-inside_op) + interior_col * inside_op, col)
-            new_col = torch.where(inside, BLACK, col)
-            print(len(t), loc.shape, new_col[0][0])
+            print(len(t), loc.shape)
             p.set_non_recursive(color=new_col)
         return obj
 
@@ -57,40 +59,36 @@ def bloch(r=1.):
         p.set_non_recursive(color=p.color.set_opacity(op))
 
     with Off():
+        Scene.get_camera().set_distance_to_screen(15)
         s1.spawn()
         s2.spawn()
         s3.spawn()
-        c1.spawn().move(IN*2.15)
+        c1.spawn().move(IN*2.3)
         s1.smoothness = 0.6
         s1.metallicness = 0.4
         center_dot.spawn()
         light_source = Scene.get_light_sources()[0]
         light_source.move_to(UP * 12)
         light_source.rotate_around_point(ORIGIN, 45, OUT)
-        light_source.rotate_around_point(ORIGIN, 35, UP)
+        light_source.rotate_around_point(ORIGIN, 20, UP)
         print('location:', light_source.location)
         print('color:', light_source.color)
         print(light_source.location.norm())
         loc = -light_source.location
         loc[...,2] *= -1
         Scene.add_light_source(PointLight(location=loc, color=Color("#444444"), opacity=0.5).spawn())
-
-    print('a')
+        interior_col_update(c1, [0.])
     c1.add_updater(interior_col_update)
-    print('b')
 
-    with Sync(run_time=0.5, rate_func=rate_funcs.identity):
-        print('c')
+    with Sync(run_time=3, rate_func=rate_funcs.identity):
         c1.move(OUT*6)
-        print('d')
-    print('e')
 
     return 'bloch'
 
 if __name__ == "__main__":
     COMPUTING_DEFAULTS.render_device = torch.device('cpu')
     COMPUTING_DEFAULTS.max_cpu_memory_used *= 6
-    quality = LD
+    quality = HD
     r = 2.
     bgcol = DARKER_GREY
     name = bloch(r)
