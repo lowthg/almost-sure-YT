7f486b9c18971fb1ead77553aa0082edc52bd264
George Lowther <37965159+lowthg@users.noreply.github.com>
2025-08-03 18:53:17 +0100

    algan anims

--- algansource/bloch.py
+++ algansource/bloch.py
@@ -19,30 +19,31 @@ def create_sphere(r=1., simple_sphere=True):
     # surface_col = Color("#333333")
     surface_col = Color("#000060")
     r2 = r * 0.995
-    s3 = ManimMob(mn.Sphere(radius=r * 1.005, fill_opacity=0, stroke_opacity=0.5, stroke_color=mn.BLACK, stroke_width=2))
+    fill_opacity = 0.4 if simple_sphere else 0
+    s3 = ManimMob(mn.Sphere(radius=r * 1.005, fill_opacity=fill_opacity, fill_color = mn.GREEN, stroke_opacity=0.5,
+                            stroke_color=mn.BLACK, stroke_width=2, resolution=(24, 12)))
     with Off():
         s3.orbit_around_point(ORIGIN, 90, RIGHT)
-        s3.orbit_around_point(ORIGIN, 7, UP)  # don't cover the center with a line
+        s3.orbit_around_point(ORIGIN, -7, UP)  # don't cover the center with a line
         s3.orbit_around_point(ORIGIN, 35, RIGHT)  # let's make the north pole point slightly towards us
         s3.spawn()
 
     a = math.log(1 - min_op_s1) * r # min opacity = 1 - exp(a/r)
     b = math.log(1 - max_op_s2) / r  # max opacity = 1 - exp(b * r)
 
-    def interior_col_update(obj: Mob, t=(0.,)):
-        print('updator called')
-        try:
-            col = obj.base_color
-        except AttributeError:
-            col = obj.color
-            obj.base_color = col
+    def interior_col_update(obj: Mob, t=(0.,), color=None):
+        # print('updator called')
+        if color is not None:
+            obj.base_color = color
+        col = obj.base_color
+
         for p in obj.get_descendants():
             loc = p.location
             inside = loc.norm(dim=-1, keepdim=True).le(r2)
             depth = (r2*r2 - loc[...,:2].square().sum(dim=-1, keepdim=True)).clamp(0).sqrt() + loc[...,2:3]
             inside_op = 1 - (depth * (b/2)).exp()
             new_col = torch.where(inside, col * (1-inside_op) + interior_col * inside_op, col)
-            print(len(t), loc.shape)
+            # print(len(t), loc.shape)
             p.set_non_recursive(color=new_col)
         return obj
 
@@ -69,37 +70,93 @@ def create_sphere(r=1., simple_sphere=True):
 
 def bloch(r=1., simple_sphere=False):
     sphere, col_update = create_sphere(r, simple_sphere)
-    c1 = Cylinder(radius=0.2, height=1, color=WHITE)
+    # c1 = Cylinder(radius=0.2, height=1, color=WHITE)
 
-    center_dot = Sphere(radius=r * 0.04, color=WHITE) # * (1-op) + interior_col * op)
-    col_update(center_dot)
+    center_dot = Sphere(radius=r * 0.04)
+    col_update(center_dot, color=WHITE)
 
     with Off():
         Scene.get_camera().set_distance_to_screen(15)
-        c1.spawn().move(IN*2.3)
+        # c1.spawn().move(IN*2.3)
         center_dot.spawn()
         light_source = Scene.get_light_sources()[0]
         light_source.move_to(UP * 12)
         light_source.rotate_around_point(ORIGIN, 45, OUT)
         light_source.rotate_around_point(ORIGIN, 20, UP)
-        print('location:', light_source.location)
-        print('color:', light_source.color)
-        print(light_source.location.norm())
+        # print('location:', light_source.location)
+        # print('color:', light_source.color)
+        # print(light_source.location.norm())
         loc = -light_source.location
         loc[...,2] *= -1
         Scene.add_light_source(PointLight(location=loc, color=Color("#444444"), opacity=0.5).spawn())
-        col_update(c1, [0.])
-    c1.add_updater(col_update)
+        # col_update(c1, [0.])
 
-    with Sync(run_time=3, rate_func=rate_funcs.identity):
-        c1.move(OUT*6)
+    # c1.add_updater(col_update)
+
+    # with Sync(run_time=3, rate_func=rate_funcs.identity):
+        # c1.move(OUT*6)
+
+    s_up = sphere.get_forward_direction()
+    s_right = sphere.get_right_direction()
+    s_forward = sphere.get_upwards_direction()
+    mn_up = s_up.numpy()[0,0,:]
+    mn_right = s_right.numpy()[0,0,:]
+    mn_forward = s_forward.numpy()[0,0,:]
+
+
+    print(s_up)
+    print(s_right)
+    print(s_forward)
+
+    equp = ManimMob(mn.MathTex(r'\lvert{\rm up}\rangle', font_size=32))
+    eqdn = ManimMob(mn.MathTex(r'\lvert{\rm down}\rangle', font_size=32))
+    eqrt = ManimMob(mn.MathTex(r'\lvert{\rm right}\rangle', font_size=32))
+    eqlt = ManimMob(mn.MathTex(r'\lvert{\rm left}\rangle', font_size=32))
+    eqft = ManimMob(mn.MathTex(r'\lvert{\rm front}\rangle', font_size=32))
+    eqbk = ManimMob(mn.MathTex(r'\lvert{\rm back}\rangle', font_size=32))
+    equp.move_to(s_up * r * 1.1)
+    eqdn.move_to(-s_up * r * 1.35)
+    eqrt.move_to(s_right * r * 1.25)
+    eqlt.move_to(-s_right * r * 1.2)
+    eqft.move_to(s_forward * r * 1.05)
+    eqbk.move_to(-s_forward * r * 1.05)
+    # xAx = Line(-s_right * r, s_right * r, color=WHITE, border_width=1)
+    #xAx = ManimMob(mn.Line(-mn_right * r, mn_right * r, stroke_width=2))
+    # yAx = Line(-s_up * r, s_up * r, border_width=1)
+    #yAx = ManimMob(mn.Line(-mn_up * r, mn_up * r, stroke_width=2))
+    # zAx = Line(-s_forward * r*0, s_forward * r, border_width=1)
+    #zAx = ManimMob(mn.Line(-mn_forward * r*0.5, mn_forward * r * 0.99, stroke_width=1))
+    axes = Group(
+        ManimMob(mn.Line(ORIGIN, mn_forward * r * 0.99, stroke_width=1)),
+        ManimMob(mn.Line(ORIGIN, -mn_forward * r * 0.99, stroke_width=1)),
+        ManimMob(mn.Line(ORIGIN, mn_up * r * 0.99, stroke_width=1)),
+        ManimMob(mn.Line(ORIGIN, -mn_up * r * 0.99, stroke_width=1)),
+        ManimMob(mn.Line(ORIGIN, mn_right * r * 0.99, stroke_width=1)),
+        ManimMob(mn.Line(ORIGIN, -mn_right * r * 0.99, stroke_width=1))
+    )
+    col_update(axes, color=WHITE)
+    #col_update(yAx, color=WHITE)
+    #col_update(zAx, color=WHITE)
+
+    with Sync():
+        equp.spawn()
+        eqdn.spawn()
+        eqrt.spawn()
+        eqlt.spawn()
+        eqft.spawn()
+        eqbk.spawn()
+        #xAx.spawn()
+        #yAx.spawn()
+        #zAx.spawn()
+        axes.spawn()
+
 
     return 'bloch'
 
 if __name__ == "__main__":
     COMPUTING_DEFAULTS.render_device = torch.device('cpu')
     COMPUTING_DEFAULTS.max_cpu_memory_used *= 6
-    quality = LD
+    quality = HD
     r = 2.
     bgcol = DARKER_GREY
     name = bloch(r)
