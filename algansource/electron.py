f867bdb1eaba0e8af15c7d11030d2c0d19116d29
George Lowther <37965159+lowthg@users.noreply.github.com>
2025-08-08 09:32:34 +0100

    algan anims

--- algansource/electron.py
+++ algansource/electron.py
@@ -21,16 +21,27 @@ dir_angle = {
     UP: 0,
     DOWN: 180,
     RIGHT: 90,
-    LEFT: 270
+    LEFT: -90
 }
 
-def create_electron(r=1, bloom=True) -> tuple[Mob, Mob]:
+# def generate_checkerboard
+def create_electron(r=1, bloom=True, sub_n=5) -> tuple[Mob, Mob]:
     r1 = r * 0.5
     r2 = r * 1.5
     r4 = r * 0.8
     r5 = r * 1.5
     r_max = 2.5 * r
-    s1 = ManimMob(mn.Sphere(radius=r1, stroke_opacity=0, resolution=[8, 4], checkerboard_colors=[mn.WHITE, mn.YELLOW]))
+    s0 = mn.Sphere(radius=r1, stroke_opacity=1, resolution=[8 * sub_n, 4 * sub_n], checkerboard_colors=[mn.WHITE, mn.YELLOW])
+    for i, x in enumerate(s0.submobjects):
+        k, j = divmod(i, 4 * sub_n)
+        if (k // sub_n) % 2 == (j // sub_n) % 2:
+            x.set(color=mn.YELLOW)
+        else:
+            x.set(color=mn.WHITE)
+    s1 = ManimMob(s0)
+    # s1_1 = Sphere(radius=r1, grid_height=20)
+    # s1 = ImageMob('WorldMap.png').spawn()
+    # s1.set_shape_to(s1_1)
     # arr1 = ManimMob(mn.Arrow3D(start=OUT.numpy()*r1, end=OUT.numpy()*r2, thickness=0.05))
     ht = r * 0.7
     ht2 = r * 0.5
@@ -50,7 +61,7 @@ def create_electron(r=1, bloom=True) -> tuple[Mob, Mob]:
     eps = PI/n * 0.3  # little shift to stop stuff lining up
     for r3, dth in ((r5, PI/n + eps), (r4, eps)):
         th1 = math.asin(r1 / (2 * r3)) * 2
-        arc = ManimMob(mn.Arc(radius=r3, stroke_color=mn.PURE_BLUE, stroke_width=sw, start_angle=th1 + PI,
+        arc = ManimMob(mn.Arc(radius=r3, stroke_color=mn.BLUE_D, stroke_width=sw, start_angle=th1 + PI,
               angle=2*PI-2*th1).rotate(PI/2, RIGHT.numpy(), about_point=ORIGIN.numpy()).shift(RIGHT.numpy()*r3),
                        texture_grid_size=5)
         fl += [arc.clone().orbit_around_point(ORIGIN, (2*PI*i/n + dth) * RADIANS, OUT) for i in range(n)]
@@ -71,27 +82,35 @@ def create_electron(r=1, bloom=True) -> tuple[Mob, Mob]:
 
     return Group(s1, arr1), Group(*efl, *fl)
 
-def electron(start=UP, end=RIGHT, r=1., show_field=False):
-    bare, field = create_electron(r, True)
-    elec = Group(bare, field) if show_field else Group(bare)
+def electron(start=UP, end=RIGHT, r=1., show_field=False, sub_n=5):
+    bare, field = create_electron(r, sub_n=sub_n)
+    dressed = Group(bare, field)
+
+    if (start == DOWN).all() and (end == UP).all():
+        d_angle = -180
+    else:
+        d_angle = dir_angle[end] - dir_angle[start]
+        if d_angle < -180:
+            d_angle += 360
+        elif d_angle > 180:
+            d_angle -= 360
 
     th1 = 20 * PI / 180
-    d_angle = dir_angle[end] - dir_angle[start]
-    if d_angle <= -180:
-        d_angle += 360
-    elif d_angle > 180:
-        d_angle -= 360
 
     with Off():
-        elec.orbit_around_point(ORIGIN, th1 * RADIANS, RIGHT)
-        elec_back = elec.get_upwards_direction()
-        elec.orbit_around_point(ORIGIN, dir_angle[start], elec_back)
+        dressed.orbit_around_point(ORIGIN, th1 * RADIANS, RIGHT)
+        elec_back = bare.get_upwards_direction()
+        dressed.orbit_around_point(ORIGIN, dir_angle[start], elec_back)
         Scene.get_camera().set_euler_angles(90, 0, 0)
-        elec.spawn()
+        bare.spawn()
+        if show_field:
+            field.spawn()
 
     elec_pos = ORIGIN
 
-    elec_up = -elec.get_forward_direction()
+    elec = Group(bare, field) if show_field else bare
+
+    elec_up = -bare.get_forward_direction()
     with Sync(run_time=2, same_run_time=True, rate_func=rate_funcs.identity):
         elec.orbit_around_point(elec_pos, 360, elec_up)
         if d_angle != 0:
@@ -106,10 +125,12 @@ def electron(start=UP, end=RIGHT, r=1., show_field=False):
 
 if __name__ == "__main__":
     COMPUTING_DEFAULTS.render_device = torch.device('cpu')
+    COMPUTING_DEFAULTS.max_cpu_memory_used *= 6
     quality = HD
-    show_field = True
-    bgcol = BLACK
+    show_field = False
+    bgcol = TRANSPARENT
     big = False
+    sub_n=5
 
     r = 1. if big else 0.5
 
@@ -121,7 +142,11 @@ if __name__ == "__main__":
     else:
         bloom_new = partial(bloom_filter, num_iterations=7, kernel_size=kernel_size, strength=15, scale_factor=6)
 
-    for start, end in [(UP, DOWN)]:
-        name = electron(start, end, r=r, show_field=show_field)
+    # orient = [(UP, DOWN), (DOWN, UP), (UP, UP), (DOWN, DOWN)]
+    orient = [(UP, UP), (DOWN, DOWN), (UP, DOWN), (DOWN, UP)]
+    orient += [(LEFT, LEFT), (RIGHT, LEFT), (UP, RIGHT), (RIGHT, RIGHT)]
+
+    for start, end in orient:
+        name = electron(start, end, r=r, show_field=show_field, sub_n=5)
         render_to_file(name, render_settings=quality, post_processes = [bloom_new], background_color=bgcol)
         # render_to_file(name, render_settings=quality, background_color=TRANSPARENT, file_extension='mov')
