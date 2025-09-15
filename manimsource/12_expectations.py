86fe7b5bcecab9a1be503ea531779eed40404eef
George Lowther <37965159+lowthg@users.noreply.github.com>
2025-09-16 00:40:38 +0100

    expectations

--- manimsource/12_expectations.py
+++ manimsource/12_expectations.py
@@ -11,6 +11,88 @@ H = LabeledDot(Text("H", color=BLACK, font='Helvetica', weight=SEMIBOLD), radius
 T = LabeledDot(Text("T", color=BLACK, font='Helvetica', weight=SEMIBOLD), radius=0.35, color=YELLOW).scale(1.5)
 
 
+_dice_faces = None
+
+
+def get_dice_faces(color=WHITE, dot_color=BLACK):
+    global _dice_faces
+    if _dice_faces is None:
+        blank = RoundedRectangle(width=2, height=2, fill_color=color, fill_opacity=1, corner_radius=0.2, stroke_color=GREY)
+        dot = Dot(radius=0.22, color=dot_color, z_index=1)
+        x = RIGHT * 0.54
+        y = UP * 0.54
+
+        _dice_faces = []
+
+        for dots in [
+            [ORIGIN],
+            [-x - y, x + y],
+            [-x - y, ORIGIN, x + y],
+            [-x - y, -x + y, x + y, x - y],
+            [-x - y, -x + y, x + y, x - y, ORIGIN],
+            [-x - y, -x, -x + y, x - y, x, x + y]
+        ]:
+            _dice_faces.append(VGroup(blank.copy(), *[dot.copy().move_to(s) for s in dots]))
+    return _dice_faces
+
+
+
+def animate_roll(scene, key, pos=ORIGIN, scale=0.3, right=False, slide=True):
+    if isinstance(pos, Mobject):
+        pos = pos.get_center()
+    key = int(key) - 1
+    rows = [
+        [1, 5, 6, 2],
+        [2, 4, 5, 3], ##
+        [3, 1, 4, 6],  ##
+        [4, 2, 3, 5], #
+        [5, 6, 2, 1], #
+        [6, 5, 1, 2],  ##
+    ]
+
+    faces = get_dice_faces()
+    f_row = [faces[i-1] for i in rows[key]]
+
+    flag = False
+    for i in range(10, -1, -1):
+        t = -i * i * 0.045
+        c = math.cos(t) * scale
+        s = math.sin(t) * scale
+        if slide:
+            d0 = math.floor(2*t/math.pi)
+            d = (d0 + math.sin(t - math.pi * d0/2)) * scale * 2
+        else:
+            d = 0
+        if right:
+            arr = [f_row[0].copy().apply_matrix([[c, 0], [0, scale]]).move_to(pos + RIGHT * (s+d)),
+                   f_row[1].copy().apply_matrix([[s, 0], [0, scale]]).move_to(pos + LEFT * (c-d)),
+                   f_row[2].copy().apply_matrix([[-c, 0], [0, scale]]).move_to(pos + LEFT * (s-d)),
+                   f_row[3].copy().apply_matrix([[-s, 0], [0, scale]]).move_to(pos + RIGHT * (c+d))]
+        else:
+            arr = [f_row[0].copy().apply_matrix([[scale, 0], [0, c]]).move_to(pos + UP * (s+d)),
+                   f_row[1].copy().apply_matrix([[scale, 0], [0, s]]).move_to(pos + DOWN * (c-d)),
+                   f_row[2].copy().apply_matrix([[scale, 0], [0, -c]]).move_to(pos + DOWN * (s-d)),
+                   f_row[3].copy().apply_matrix([[scale, 0], [0, -s]]).move_to(pos + UP * (c+d))]
+
+        if c < 0:
+            arr[0].set_opacity(0)
+        else:
+            arr[2].set_opacity(0)
+        if s < 0:
+            arr[1].set_opacity(0)
+        else:
+            arr[3].set_opacity(0)
+        if flag:
+            for j in range(4):
+                f[j].target = arr[j]
+            scene.play(*[MoveToTarget(f[j]) for j in range(4)], rate_func=rate_functions.linear, run_time=0.05 * (1 + t / 10))
+        else:
+            f = arr
+            flag = True
+
+    scene.remove(*f[1:])
+    return f[0]
+
 class ExpectedValue(Scene):
     fs1 = 100
     def construct(self):
@@ -264,7 +346,6 @@ class AliceExpected(Scene):
         eq4 = MathTex(r'\mathbb E[{\rm Alice receives}\vert a]', r'=', r'\sum_{b=0}^{a-1}4^b\left(\frac12\right)^{b+1}')
         eq4.move_to(VGroup(eq1, eq2, eq3), coor_mask=UP)
         VGroup(eq4[0][-2]).set_color(RED)
-        VGroup(eq4[2][-3], eq4[2][1], eq4[2][3]).set_color(BLUE)
         eq4_1 = MathTex(r'\mathbb E[{\rm Alice receives}\vert a]', r'=', r'\sum_b4^b')
         mh.align_sub(eq4_1, eq4_1[1], eq4[1])
         eq5 = MathTex(r'\mathbb E[{\rm Alice receives}\vert a]', r'=', r'\sum_{b=0}^{a-1}\left(\frac42\right)^b\frac12')
@@ -400,14 +481,12 @@ class AliceExpected(Scene):
                   mh.fade_replace(eq2[0][5:], eq20_1)
                   )
         self.remove(eq20_1)
-        self.wait(0.1)
         del1 = eq20[2][4:6]
         del2 = eq20[2][-2:]
         line1 = Line(del1.get_corner(DL), del1.get_corner(UR), stroke_width=7, stroke_color=GREEN).set_z_index(5)
         line2 = Line(del2.get_corner(DL), del2.get_corner(UR), stroke_width=7, stroke_color=GREEN).set_z_index(5)
-        self.play(Create(line1, rate_func=linear, run_time=0.75))
-        self.play(Create(line2, rate_func=linear, run_time=0.75))
-        self.wait(0.1)
+        self.play(Create(line1, rate_func=linear, run_time=0.5))
+        self.play(Create(line2, rate_func=linear, run_time=0.5))
         self.play(FadeOut(eq20[2][4:6], eq20[2][-6:], line1, line2))
         self.wait(0.1)
         self.play(mh.rtransform(eq20[:2], eq21[:2], eq20[2][:3], eq21[2][1:4], eq20[2][6], eq21[2][0]),
@@ -449,5 +528,259 @@ class AliceExpected(Scene):
         self.wait(0.1)
         self.play(FadeIn(eq43))
 
+
+        self.wait()
+
+class HalfMil(Scene):
+    def construct(self):
+        MathTex.set_default(font_size=100)
+        eq1 = MathTex(r'\frac12(\$200\,000+\$800\,000)')[0].set_z_index(1)
+        eq2 = MathTex(r'=\$500\,000').set_z_index(1)
+        eq2.next_to(eq1[3:], DOWN).align_to(eq1[8], LEFT)
+        box = SurroundingRectangle(VGroup(eq1, eq2), stroke_width=0, stroke_opacity=0, fill_color=BLACK,
+                                   fill_opacity=0.7, buff=0.2, corner_radius=0.2)
+        self.add(eq1, box)
+        self.wait(0.1)
+        self.play(FadeIn(eq2))
+        self.wait()
+
+class Gain25(Scene):
+    def __init__(self, *args, **kwargs):
+        if not config.transparent:
+            config.background_color = GREY
+        Scene.__init__(self, *args, **kwargs)
+
+    def construct(self):
+        MathTex.set_default(font_size=80)
+        eq1 = MathTex(r'{\rm Expected\ gain} ', r'=', r'\frac12A+\frac12\left(-\frac A2\right)').set_z_index(1)
+        eq2 = MathTex(r'{\rm Expected\ gain} ', r'=', r'\frac12A-\frac14A').set_z_index(1)
+        eq3 = MathTex(r'{\rm Expected\ gain} ', r'=', r'\frac14A').set_z_index(1)
+        mh.align_sub(eq2, eq2[1], eq1[1], coor_mask=UP)
+        mh.align_sub(eq3, eq3[1], eq1[1], coor_mask=UP)
+        VGroup(eq1[2][3], eq1[2][-4], eq2[2][3], eq2[2][-1], eq3[2][-1]).set_color(RED)
+        box1 = SurroundingRectangle(VGroup(eq1, eq2, eq3), fill_color=BLACK, fill_opacity=0.7, corner_radius=0.2,
+                                    stroke_width=0, stroke_opacity=0, buff=0.2)
+        self.add(eq1[0], eq1[1], box1)
+        self.wait(0.1)
+        self.play(FadeIn(eq1[2][:4]))
+        self.wait(0.1)
+        self.play(FadeIn(eq1[2][4:]))
+        self.wait(0.1)
+        eq2_1 = eq2[2][7].copy()
+        self.play(mh.rtransform(eq1[:2], eq2[:2], eq1[2][:4], eq2[2][:4],
+                                eq1[2][9], eq2[2][4], eq1[2][10], eq2[2][-1],
+                                eq1[2][5:7], eq2[2][5:7]),
+                  mh.fade_replace(eq1[2][11], eq2[2][6]),
+                  FadeOut(eq1[2][4], target_position=eq2[2][4]),
+                  mh.fade_replace(eq1[2][7], eq2[2][7]),
+                  mh.fade_replace(eq1[2][12], eq2_1),
+                  FadeOut(eq1[2][8], eq1[2][-1]),
+                  )
+        self.remove(eq2_1)
+        self.wait(0.1)
+        eq3_1 = eq3[2][2].copy()
+        self.play(mh.rtransform(eq2[:2], eq3[:2], eq2[2][5:], eq3[2][:4]),
+                  mh.rtransform(eq2[2][:2], eq3[2][:2], eq2[2][3], eq3[2][3]),
+                  mh.fade_replace(eq2[2][2], eq3_1),
+                  FadeOut(eq2[2][4], target_position=eq3[2][1]),
+                  run_time=1.4
+                  )
+        self.remove(eq3_1)
+        self.wait()
+
+class DifferentCalc(Gain25):
+    def construct(self):
+        MathTex.set_default(font_size=100)
+        eq1 = MathTex(r'\mathbb E[B\vert A]', r'>', r'A', r'\textsf{\textit{switch!}}').set_z_index(1)
+        eq2 = MathTex(r'\mathbb E[A\vert B]', r'>', r'B', r"\textsf{\textit{stick!}}").set_z_index(1)
+        eq3 = MathTex(r'\mathbb E[B\vert A+B]', r'=', r'\mathbb E[A\vert A+B]').set_z_index(1)
+
+        VGroup(eq1[0][-2], eq1[2][0], eq2[0][2], eq3[0][-4], eq3[2][2], eq3[2][-4]).set_color(RED)
+        VGroup(eq1[0][2], eq2[0][-2], eq2[2][0], eq3[0][2], eq3[0][-2], eq3[2][-2]).set_color(BLUE)
+
+        eq1[:-1].move_to(ORIGIN).shift(LEFT)
+        eq1[-1].next_to(eq1[:-1], RIGHT, buff=0.6, coor_mask=RIGHT).set_color(YELLOW)
+        mh.align_sub(eq2, eq2[:-1], eq1[:-1]).next_to(eq1, DOWN, coor_mask=UP)
+        eq2[-1].next_to(eq2[:-1], RIGHT, buff=0.6, coor_mask=RIGHT).set_color(YELLOW)
+        gp1 = VGroup(eq1.copy(), eq2).move_to(ORIGIN, coor_mask=UP)
+        eq3.next_to(gp1, DOWN, coor_mask=UP)
+        gp2 = VGroup(gp1[0].copy(), gp1[1].copy(), eq3).move_to(ORIGIN, coor_mask=UP)
+        box1 = SurroundingRectangle(VGroup(gp1, gp2), fill_color=BLACK, fill_opacity=0.7, corner_radius=0.2,
+                                    buff=0.2, stroke_width=0, stroke_opacity=0)
+
+        self.add(eq1[:-1], box1)
+        self.wait(0.1)
+        self.play(FadeIn(eq1[-1]))
+        eq1_1 = eq1.copy()
+        self.play(mh.transform(eq1, gp1[0]),
+                  mh.rtransform(eq1_1[0][:2], eq2[0][:2], eq1_1[0][3], eq2[0][3],
+                                eq1_1[0][-1], eq2[0][-1]),
+                  mh.fade_replace(eq1_1[0][2], eq2[0][2]),
+                  mh.fade_replace(eq1_1[0][-2], eq2[0][-2]))
+        self.wait(0.1)
+        self.play(FadeIn(eq2[1:-1]))
+        self.wait(0.1)
+        self.play(FadeIn(eq2[-1]))
+        self.wait(0.1)
+        eq2_1 = eq2.copy()
+        eq3_1 = eq3[0].copy().move_to(eq2[0], coor_mask=RIGHT)
+        self.play(mh.transform(eq1, gp2[0], eq2, gp2[1]),
+                  mh.rtransform(eq2_1[0][:2], eq3_1[:2], eq2_1[0][-2:], eq3_1[-2:], eq2_1[0][-3], eq3_1[-5]),
+                  mh.fade_replace(eq2[0][-2].copy(), eq3_1[-4]),
+                  mh.fade_replace(eq2[0][2].copy(), eq3_1[2]),
+                  FadeIn(eq3_1[-3], target_position=eq2[0][-2]),
+                  run_time=1.2)
+        self.wait(0.1)
+        eq3_2 = eq3_1.copy()
+        self.play(LaggedStart(AnimationGroup(mh.rtransform(eq3_1, eq3[0], eq3_2[:2], eq3[2][:2], eq3_2[3:], eq3[2][3:]),
+                  mh.fade_replace(eq3_2[2], eq3[2][2]), run_time=1.4),
+                  FadeIn(eq3[1]), lag_ratio=0.5))
+        self.wait(0.1)
+        eq4 = Tex(r"\sf doesn't matter", stroke_width=2, color=YELLOW)[0].move_to(VGroup(eq1, eq2), coor_mask=UP)
+        self.play(FadeOut(eq1, eq2), FadeIn(eq4))
+        self.wait()
+
+class MoneyDist(Gain25):
+    def construct(self):
+        xmax = 2.5
+        ax = Axes(x_range=[0, xmax * 1.1], y_range=[0, 1.4], x_length=8, y_length=4,
+                  axis_config={'color': WHITE, 'stroke_width': 4, 'include_ticks': False,
+                               "tip_width": 0.5 * DEFAULT_ARROW_TIP_LENGTH,
+                               "tip_height": 0.5 * DEFAULT_ARROW_TIP_LENGTH,
+                               "stroke_opacity": 1,
+                               },
+                  ).set_z_index(2)
+        def p(x):
+            if x > 0.001:
+                y = math.log(x)
+                p1 = math.exp(-y*y/2 * 5)/x
+            else: p1 = 0.
+            return p1
+        plt1 = ax.plot(p, (0, xmax), stroke_color=BLUE, stroke_width=6).set_z_index(4)
+        fill1 = ax.get_area(plt1, color=BLUE, opacity=0.5).set_z_index(3).set_stroke(opacity=0)
+
+        label1 = Tex(r'\sf\$').next_to(ax.x_axis.get_right(), UL, buff=0.2)
+        label2 = MathTex(r'p').next_to(ax.y_axis.get_top(), DR, buff=0.2)
+
+        self.add(ax, label1, label2)
+        self.wait(0.1)
+        self.play(LaggedStart(Create(plt1, run_time=1.5, rate_func=linear),
+                              FadeIn(fill1, run_time=1, rate_func=linear),
+                              lag_ratio=0.5))
+        self.wait(0.1)
+        MathTex.set_default(font_size=80)
+        pt1 = ax.coords_to_point(2.4, 0)
+        pt1_1 = ax.coords_to_point(2.4, p(2.4))
+        eq1 = MathTex(r'A', color=RED, stroke_width=2).set_z_index(10)
+        eq1.move_to(pt1, aligned_edge=UP).shift(DOWN * 0.1)
+        eq1 = VGroup(eq1, eq1.copy().set_color(WHITE).set(stroke_width=7).set_z_index(9))
+        line1 = Line(pt1, pt1_1, stroke_color=GREY, stroke_width=5).set_z_index(5)
+
+        pt2 = ax.coords_to_point(1.2, 0)
+        pt2_1 = ax.coords_to_point(1.2, p(1.2))
+        eq2 = MathTex(r'B', color=BLUE, stroke_width=2).set_z_index(10)
+        eq2.move_to(pt2, aligned_edge=UP).shift(DOWN * 0.1)
+        eq2 = VGroup(eq2, eq2.copy().set_color(WHITE).set(stroke_width=7).set_z_index(9))
+        line2 = Line(pt2, pt2_1, stroke_color=GREY, stroke_width=5).set_z_index(5)
+
+        eq3 = Tex(r'stick', color=YELLOW, font_size=100, stroke_width=2)
+        eq3.move_to(ax.coords_to_point(1.05, 0.3)).set_z_index(10)
+
+        pt3 = ax.coords_to_point(0.3, 0)
+        pt3_1 = ax.coords_to_point(0.3, p(0.3))
+        eq4 = eq1.copy().move_to(pt3, coor_mask=RIGHT)
+        pt4 = ax.coords_to_point(0.6, 0)
+        pt4_1 = ax.coords_to_point(0.6, p(0.6))
+        eq5 = eq2.copy().move_to(pt4, coor_mask=RIGHT)
+        line3  = Line(pt3, pt3_1, stroke_color=GREY, stroke_width=5).set_z_index(5)
+        line4  = Line(pt4, pt4_1, stroke_color=GREY, stroke_width=5).set_z_index(5)
+        eq6 = Tex(r'switch', color=YELLOW, font_size=100, stroke_width=2)
+        eq6.move_to(eq3).set_z_index(10)
+
+        self.play(FadeIn(eq1, line1))
+        self.wait(0.1)
+        self.play(FadeIn(eq2, target_position=eq1), FadeIn(line2))
+        self.wait(0.1)
+        self.play(FadeIn(eq3))
+        self.wait(0.1)
+        self.play(mh.rtransform(eq1, eq4), FadeIn(line3), FadeOut(eq2, line1, line2, eq3), run_time=1.5)
+        self.wait(0.1)
+        self.play(FadeIn(eq5, target_position=eq4), FadeIn(line4))
+        self.wait(0.1)
+        self.play(FadeIn(eq6))
+        self.wait()
+
+class Geometric(Gain25):
+    def show_geom(self):
+        eq1 = MathTex(r'\$1', r',\$2', r',\$4', r',\$8', r',\$16', r',\ldots\ ', r'\$2^N').set_z_index(2).to_edge(DOWN,
+                                                                                                                  buff=0.5)
+        box1 = SurroundingRectangle(eq1, corner_radius=0.2, fill_color=BLACK, fill_opacity=0.8, buff=0.2,
+                                    stroke_width=0, stroke_opacity=0)
+        eq2 = MathTex(r'N', r'\sim', r'{\rm Geometric}(p)').next_to(eq1, UP).set_z_index(2)
+        box2 = SurroundingRectangle(VGroup(eq1, eq2), corner_radius=0.2, fill_color=BLACK, fill_opacity=0.8, buff=0.2,
+                                    stroke_width=0, stroke_opacity=0)
+
+        self.add(box1)
+        for _ in eq1:
+            self.play(FadeIn(_), run_time=0.8)
+            self.wait(0.05)
+        self.wait(0.1)
+        self.play(LaggedStart(mh.rtransform(box1, box2), FadeIn(eq2), lag_ratio=0.3), run_time=1.6)
+        self.wait(0.1)
+
+        eq3 = MathTex(r'\mathbb P(N=n)', r'\sim', r'p^n').set_z_index(2).next_to(eq1, UP)
+        eq2_1 = eq2.copy().next_to(eq3, UP, coor_mask=UP)
+        box3 = SurroundingRectangle(VGroup(eq1, eq2_1), corner_radius=0.2, fill_color=BLACK, fill_opacity=0.8, buff=0.2,
+                                    stroke_width=0, stroke_opacity=0)
+        self.play(mh.rtransform(box2, box3), mh.transform(eq2, eq2_1),
+                  FadeIn(eq3), ruun_time=1.5)
+        self.wait(0.1)
+        eq4 = MathTex(r'\mathbb P(N=n)', r'=', r'p^n(1-p)').set_z_index(2)
+        mh.align_sub(eq4, eq4[0], eq3[0], coor_mask=UP)
+        self.play(mh.rtransform(eq3[0], eq4[0], eq3[2][:2], eq4[2][:2]),
+                  mh.fade_replace(eq3[1], eq4[1]),
+                  FadeIn(eq4[2][2:]))
+        self.wait(0.1)
+        return box3, eq1, eq2, eq4
+
+    def construct(self):
+        MathTex.set_default(font_size=80)
+        box1, eq1, eq2, eq3 = self.show_geom()
+        self.play(FadeOut(eq1, eq2, eq3))
+
+        eq4 = Tex(r'$N$:\ ', r'$0$').set_z_index(2).move_to(eq2).align_to(eq1, RIGHT).shift(LEFT*0.8).set_z_index(4)
+        eq5_1 = MathTex(r'\$01')[0].set_z_index(2).next_to(eq4, DOWN, buff=0.9).set_z_index(4)
+        box2 = SurroundingRectangle(eq5_1, fill_opacity=0, stroke_opacity=1, stroke_width=8, stroke_color=WHITE,
+                                    buff=0.4).set_z_index(1)
+
+        eq5 = MathTex(r'\$1')[0].move_to(box2).set_z_index(4)
+        mh.align_sub(eq5, eq5[0], eq5_1[0], coor_mask=UP)
+
+        self.play(FadeIn(eq4, eq5, box2))
+
+        pos = eq2.get_left() + LEFT * 0.3
+
+        rolls = [5, 1, 4, 6]
+        dice = []
+        eq4_2 = eq4[1]
+        eq5_2 = eq5
+        for i in range(len(rolls)):
+            dice.append(animate_roll(self, rolls[i], pos=pos + i * RIGHT * 1.4, scale=0.45, right=False, slide=True))
+            self.wait(0.1)
+            if rolls[i] == 6:
+                break
+            eq4_1 = Tex(r'${}$'.format(i+1)).set_z_index(2).move_to(eq4[1][0]).align_to(eq4[1][0], LEFT).set_z_index(4)
+            eq5_1 = MathTex(r'\${}'.format(2**(i+1)))[0].move_to(box2).set_z_index(4)
+            mh.align_sub(eq5_1, eq5_1[0], eq5_2[0])
+            self.play(FadeOut(eq4_2), FadeIn(eq4_1),
+                      mh.rtransform(eq5_2[0], eq5_1[0]),
+                      FadeOut(eq5_2[1:]), FadeIn(eq5_1[1:]))
+            eq4_2 = eq4_1
+            eq5_2 = eq5_1
+            self.wait(0.1)
+
+        eq6 = MathTex(r'\mathbb P(N=n)', r'=', r'\left(\frac56\right)^n\frac16', font_size=60).set_z_index(4)
+        eq6.set_z_index(2).align_to(eq1, LEFT).align_to(box2, UP).shift(RIGHT*0.2)
+        self.play(FadeIn(eq6))
 
         self.wait()
